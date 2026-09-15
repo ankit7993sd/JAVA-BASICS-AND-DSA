44f17c3030331019e43f51f6cfa018bd717daa7b
Ankit Dhayal <dhayalankit73@gmail.com>
2026-09-15 10:17:32 +0530

    Initial commit

---
+++ area_of_circle.java
@@ -0,0 +1,7 @@
+public class area_of_circle{
+    public static void main(String[] args){
+        float raduis = 15;
+        float area = 3.14f*raduis*raduis ;
+        System.out.println(area);
+    }
+}
---
+++ datatype.java
@@ -0,0 +1,22 @@
+public class datatype {
+    public static void main(String[] args) {
+
+        byte age = 15;
+        short year = 2026;
+        int salary = 50000;
+        long population = 90000000000l;
+        float marks = 99.5f;
+        double percentage = 95.6789;
+        char grade = 'A';
+        boolean passed = true;
+        
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(salary);
+        System.out.println(population);
+        System.out.println(marks);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
