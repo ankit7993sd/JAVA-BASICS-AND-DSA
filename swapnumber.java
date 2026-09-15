26e038ebe3147095d27140c761d3db5bc43ae847
Ankit Dhayal <dhayalankit73@gmail.com>
2026-09-15 12:11:30 +0530

    Your message here

---
+++ swapnumber.java
@@ -0,0 +1,14 @@
+public class swapnumber {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 6;
+        int temp =a;
+        System.out.println("Before swap "+"A = "+a +" And B = " +b);
+
+        int a = b;
+        int b = temp;
+         System.out.println("After swap "+"A = "+a +" And B = " +b);
+
+    }
+    
+}
