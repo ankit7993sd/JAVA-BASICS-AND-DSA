194a53942473d5fa127b7c5e5560def5c61ca422
Ankit Dhayal <dhayalankit73@gmail.com>
2026-09-18 12:31:02 +0530

    your commit message

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter your number");
+        int num = Sc.nextInt();
+        
+        for (int i =1 ;i<=10 ;i++)
+        System.out.println(num +"*"+i +"=" +i * num);
+
+}
+    }
+
+
+    
--- comp.java
+++ comp.java
@@ -1,76 +1,100 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
-import java.util.Scanner;
+import java.util.*;
 
 public class  comp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your age ");
-        int age = sc.nextInt();
-        
-          if(age<18){
-        System.out.print("Focus on your studies");
-            }
-        else{
-        int compatibilty =0;
-        System.out.print("Enter your confidence level 1-10 ");
-        int confidence = sc.nextInt();
-        if (confidence>=7){
-            compatibilty +=1;
-        }
-            else{
-                System.out.println("Your confidence is low😂😂");
-            }
-        System.out.print("Enter your Effort evel 1-10 ");
-        int effort = sc.nextInt();
-            if (effort>=7){
-            compatibilty +=1;
-        }
-            else{
-                System.out.println("Your Effort is low😂😂");
-            }
-        System.out.print("Enter number of repiles ");
-        int repiles = sc.nextInt();
-            if (repiles>=7){
-            compatibilty +=1;
-        }
-            else{
-                System.out.println("You are getting ignored😂😂");
-            }
-
-        System.out.println("Your complatibilty score is "+compatibilty); 
-            if(compatibilty == 3){
-                System.out.println("Legendary Match");
-            }
-            else if(compatibilty == 2){
-                System.out.println("Good match");
-            }
-              
-            else if(compatibilty == 1){
-                System.out.println("Risky  match");
-            }
-            else;{
-                System.out.println("Bro is Staying single");
-            }
-
-            System.out.println("1. Send Messages");
-            System.out.println("2.Wait for Reply");
-            System.out.println("3.Move on ");
-            int reply =sc.nextInt();
-            if (reply == 1){
-                System.out.println("Sending message.....");
     
-            }
-            else if (reply == 2){
-                System.out.println("Waiting for reply.....");
-            }
-            else if (reply == 3){
-                System.out.println("Right decision");
-            }
-            else{
-                System.out.print("Invialid choice");
-            }
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter your age ");
+    //     int age = sc.nextInt();
+        
+    //       if(age<18){
+    //     System.out.print("Focus on your studies");
+    //         }
+    //     else{
+    //     int compatibilty =0;
+    //     System.out.print("Enter your confidence level 1-10 ");
+    //     int confidence = sc.nextInt();
+    //     if (confidence>=7){
+    //         compatibilty +=1;
+    //     }
+    //         else{
+    //             System.out.println("Your confidence is low😂😂");
+    //         }
+    //     System.out.print("Enter your Effort evel 1-10 ");
+    //     int effort = sc.nextInt();
+    //         if (effort>=7){
+    //         compatibilty +=1;
+    //     }
+    //         else{
+    //             System.out.println("Your Effort is low😂😂");
+    //         }
+    //     System.out.print("Enter number of repiles ");
+    //     int repiles = sc.nextInt();
+    //         if (repiles>=7){
+    //         compatibilty +=1;
+    //     }
+    //         else{
+    //             System.out.println("You are getting ignored😂😂");
+    //         }
+
+    //     System.out.println("Your complatibilty score is "+compatibilty); 
+    //         if(compatibilty == 3){
+    //             System.out.println("Legendary Match");
+    //         }
+    //         else if(compatibilty == 2){
+    //             System.out.println("Good match");
+    //         }
+              
+    //         else if(compatibilty == 1){
+    //             System.out.println("Risky  match");
+    //         }
+    //         else;{
+    //             System.out.println("Bro is Staying single");
+    //         }
+
+    //         System.out.println("1. Send Messages");
+    //         System.out.println("2.Wait for Reply");
+    //         System.out.println("3.Move on ");
+    //         int reply =sc.nextInt();
+    //         if (reply == 1){
+    //             System.out.println("Sending message.....");
+    
+    //         }
+    //         else if (reply == 2){
+    //             System.out.println("Waiting for reply.....");
+    //         }
+    //         else if (reply == 3){
+    //             System.out.println("Right decision");
+    //         }
+    //         else{
+    //             System.out.print("Invialid choice");
+    //         }
       
-    }
+    // }
+// }
+Scanner sc = new Scanner(System.in);
+System.out.print
+int day = sc.nextInt();
+
+
+
+switch (day) {
+    case 1:
+        System.out.println("Pizza");
+        break;
+
+    case 2:
+        System.out.println("Burger");
+        break;
+
+    case 3:
+        System.out.println("Pasta");
+        break;
+
+    default:
+        System.out.println("Invalid choice");
+}
 }
 }
