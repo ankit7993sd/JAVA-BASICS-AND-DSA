9d6c71c9924ab2fe23b433757f2f8f0b6380ecf8
Ankit Dhayal <dhayalankit73@gmail.com>
2026-09-21 13:00:27 +0530

    your commit message

--- oddeven.java
+++ oddeven.java
@@ -1,5 +1,5 @@
 public class oddeven {
-    static boolean oddeven(int a ){
+    static boolean oddevens(int a ){
         if (a%2==0)
             return true;
         else
@@ -7,7 +7,7 @@ public class oddeven {
 
     }
     public static void main(String[] args){
-       System.out.println("Is the number odd: " + oddeven(4));
+       System.out.println("Is the number odd ? " + oddevens(4));
     }
     
 }
