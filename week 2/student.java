6e6a07673e6ece35bec562789293b9a3f9da0750
Ankit Dhayal <dhayalankit73@gmail.com>
2026-09-21 10:53:54 +0530

    your commit message

--- week 2/student.java
+++ week 2/student.java
@@ -1,8 +1,25 @@
 public class student {
+      static void greet() {
+            System.out.println("Hello ");
+        }
+        static int sum(){
+            int a,b;
+            int sum = a+b;
+            return sum;
+            
+        }
+        System.out.println("Sum + "+sum(19,20));
     
     static void main(String[] args){
         String name = "Ankit";
+        int Age = 20;
+        String Branch = "BTECH";
         System.out.print("Hello "+name);
+         System.out.print("Age "+Age);
+          System.out.print("Branch " +Branch);
+
+        student.greet();
+
     }
     
 }
